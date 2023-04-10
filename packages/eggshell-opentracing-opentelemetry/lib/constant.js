6523ab5d55b1172c59feddc9dcbbceb2eaa42ec3
Lin Qun <jinasonlin@gmail.com>
2023-04-10 11:04:34 +0800

    chore: update traceUrl config

--- packages/eggshell-opentracing-opentelemetry/lib/constant.js
+++ packages/eggshell-opentracing-opentelemetry/lib/constant.js
@@ -4,5 +4,6 @@ exports.traceUrl = {
   local: 'http://172.28.34.35:4318/v1/traces',
   test: 'http://otel-collector-http-test.za-tech.net/v1/traces',
   uat: 'http://otel-collector-http-pre.za-tech.net/v1/traces',
+  pre: 'http://otel-collector-http-pre.za-tech.net/v1/traces',
   prd: 'http://otel-collector-http-pub.za-tech.net/v1/traces',
 };
